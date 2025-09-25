7890d0c690bcfb75cc03ea244ce13796b4e73d99
Dijana Lazic <shadow77aj@yahoo.com>
2025-09-25 18:38:21 +0200

    gui

--- gui.py
+++ gui.py
@@ -4,9 +4,8 @@ label=sg.Text("Type in a todo")
 input_box=sg.InputText(tooltip="Enter a todo")
 add_button=sg.Button("Add")
 window=sg.Window("My todo app",layout=[[label],
-                                            [input_box],
-                                       [add_button]
-                                       ])
+                                            [input_box,add_button]
+                                           ])
 window.read()
 print("Hello")
 window.close()
