c7ae4e5328b80c7b33037151b2e431cf89e2ee77
Dijana Lazic <shadow77aj@yahoo.com>
2025-10-04 14:06:49 +0200

    implement delete and exit buttons

--- gui.py
+++ gui.py
@@ -6,9 +6,14 @@ add_button=sg.Button("Add")
 list_box=sg.Listbox(values=functions.get_todos("todos.txt"), key="todos",
                     enable_events=True, size=(45,10))
 edit_button=sg.Button("Edit")
+delete_button=sg.Button("Delete")
+exit_button=sg.Button("Exit")
 window=sg.Window("My todo app",
-                 layout=[[label], [input_box, add_button],[list_box, edit_button]],
-                 font=("Helvetica",20))
+                 layout=[[label],
+                         [input_box, add_button],
+                         [list_box, edit_button, delete_button],
+                         [exit_button]],
+                 font=('Helvetica', 20))
 while True:
     event, values = window.read()
     if event in(sg.WIN_CLOSED,None):
@@ -27,6 +32,7 @@ while True:
                 todos.append(new_todo)
                 functions.write_todos(todos,"todos.txt")
                 window["todos"].update(values=todos)
+                window["todo"].update(value='')
         case "Edit":
             todo_to_edit=values["todos"][0]
             new_todo=values["todo"].strip()
@@ -37,6 +43,15 @@ while True:
             window["todos"].update(values=todos)
         case "todos":
             window["todo"].update(value=values["todos"][0])
+        case "Delete":
+            todo_to_delete=values["todos"][0]
+            todos=functions.get_todos("todos.txt")
+            todos.remove(todo_to_delete)
+            functions.write_todos(todos)
+            window["todos"].update(values=todos)
+            window["todo"].update(value='')
+        case "Exit":
+            break
         case sg.WIN_CLOSED:
             break
 window.close()
