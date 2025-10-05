49df51a2274460b9d9eea9503c706176f3b2662b
Dijana Lazic <shadow77aj@yahoo.com>
2025-10-05 16:16:57 +0200

    add color

--- gui.py
+++ gui.py
@@ -1,5 +1,10 @@
+import time
 import functions
 import FreeSimpleGUI as sg
+sg.theme("LightBrown5")
+
+now=time.strftime("%b %d, %Y in %H:%M:%S")
+clock=sg.Text("", key="clock")
 label=sg.Text("Type in a todo")
 input_box=sg.InputText(tooltip="Enter a todo", key="todo")
 add_button=sg.Button("Add")
@@ -9,21 +14,22 @@ edit_button=sg.Button("Edit")
 delete_button=sg.Button("Delete")
 exit_button=sg.Button("Exit")
 window=sg.Window("My todo app",
-                 layout=[[label],
+                 layout=[[clock],
+                         [label],
                          [input_box, add_button],
                          [list_box, edit_button, delete_button],
                          [exit_button]],
                  font=('Helvetica', 20))
 while True:
-    event, values = window.read()
+    event, values = window.read(timeout=100)
+    window["clock"].update(value=time.strftime("%b %d, %Y in %H:%M:%S"))
     if event in(sg.WIN_CLOSED,None):
         break
-    if not isinstance(values,dict):
-        continue
-    event,values=window.read()
+
     print(1,event)
     print(2,values)
     print(3,values["todos"])
+
     match event:
         case "Add":
             todos=functions.get_todos("todos.txt")
@@ -34,22 +40,29 @@ while True:
                 window["todos"].update(values=todos)
                 window["todo"].update(value='')
         case "Edit":
-            todo_to_edit=values["todos"][0]
-            new_todo=values["todo"].strip()
-            todos=functions.get_todos("todos.txt")
-            index = todos.index(todo_to_edit)
-            todos[index]=new_todo
-            functions.write_todos(todos,filepath="todos.txt")
-            window["todos"].update(values=todos)
+            try:
+                todo_to_edit=values["todos"][0]
+                new_todo=values["todo"].strip()
+                todos=functions.get_todos("todos.txt")
+                index = todos.index(todo_to_edit)
+                todos[index]=new_todo
+                functions.write_todos(todos,filepath="todos.txt")
+                window["todos"].update(values=todos)
+                window["todo"].update(value='')
+            except IndexError:
+                sg.popup("Select the item first", font=("Helvetica", 20))
         case "todos":
             window["todo"].update(value=values["todos"][0])
         case "Delete":
-            todo_to_delete=values["todos"][0]
-            todos=functions.get_todos("todos.txt")
-            todos.remove(todo_to_delete)
-            functions.write_todos(todos)
-            window["todos"].update(values=todos)
-            window["todo"].update(value='')
+            try:
+                todo_to_delete=values["todos"][0]
+                todos=functions.get_todos("todos.txt")
+                todos.remove(todo_to_delete)
+                functions.write_todos(todos)
+                window["todos"].update(values=todos)
+                window["todo"].update(value='')
+            except IndexError:
+                sg.popup("Missing item to delete", font=("Helvetica", 20))
         case "Exit":
             break
         case sg.WIN_CLOSED:
